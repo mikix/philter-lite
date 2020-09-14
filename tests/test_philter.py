9a8691a17857f0346338d230a0906898099aff0b
Tim Orme <tim@sironamedical.com>
2020-09-14 11:02:59 -0700

    Fix black

--- tests/test_philter.py
+++ tests/test_philter.py
@@ -1,6 +1,8 @@
-from philter_lite import philter
 import os
+
 import pytest
+
+from philter_lite import philter
 
 
 def test_filter_from_dict():
@@ -9,22 +11,29 @@ def test_filter_from_dict():
         "type": "regex",
         "filepath": "filters/regex/addresses/city.txt",
         "exclude": "test_ex",
-        "notes": "test_notes"
+        "notes": "test_notes",
     }
 
     filter = philter.filter_from_dict(filter_dict)
 
     assert filter.type == "regex"
     assert filter.title == "test_city"
-    assert filter.filepath == os.path.abspath(os.path.join(os.path.dirname(__file__), "../philter_lite", "filters/regex/addresses/city.txt"))
+    assert filter.filepath == os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            "../philter_lite",
+            "filters/regex/addresses/city.txt",
+        )
+    )
     assert filter.exclude == "test_ex"
     assert filter.notes == "test_notes"
     assert isinstance(filter, philter.RegexFilter)
 
+
 def test_filter_from_dict_missing_file():
     filter_dict = {
         "type": "regex",
-        "filepath": "filters/regex/addresses/non_existent.txt"
+        "filepath": "filters/regex/addresses/non_existent.txt",
     }
 
     with pytest.raises(Exception):
